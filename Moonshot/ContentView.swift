b05a726e933e153255f35082ed8abc205b0a687b
raster-gen <rasterroot@gmail.com>
2022-11-17 06:18:34 +0000

    decode data from json. Added castom colors. darkmode

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,10 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
         NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(missions){ mission in
+                        NavigationLink{
+                            Text("Detail View \(mission.displayName)")
+                        } label: {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.lightBackground))
+                            
+                        }
+                        
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
         
     }
